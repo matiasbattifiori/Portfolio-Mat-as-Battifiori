fce48ecfd30e0a390bc7fa4d3e760f7f9486a22b
matiasbattifiori <matiasbattifiori@gmail.com>
2025-02-10 15:42:44 -0300

    Delete src directory

--- src/Components/Asincronía/Asincronía.jsx
+++
@@ -1,9 +0,0 @@
-const mostrarData=async()=>{
-    await fetch ("https://rickandmortyapi.com/api/character")
-        .then((resp)=>{return resp.json()})
-        .then((data)=>{info=data;})
-        .catch((error)=>console.log("error:" +error));
-
-        console.log (info)
-        mostrarInfo(info.results)
-}
